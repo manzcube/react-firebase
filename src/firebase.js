f36c199a7ea144f191f63874fe374bf87e5650f1
Marc Alzamora <marcalzamora1@hotmail.com>
2022-06-21 21:27:04 +0200

    crud & auth done

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+// Functions from the SDKs
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+// Firebase configuration
+const firebaseConfig = {
+  
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app)
+
+export const auth = getAuth()
